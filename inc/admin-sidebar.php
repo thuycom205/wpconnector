dde703372d8a7300e22a2bda649bb0915994ef51
Luu Thuy <luuthuy205@gmail.com>
2017-03-27 10:19:27 +0700

    i

---
+++ inc/admin-sidebar.php
@@ -0,0 +1,17 @@
+<?php global $jck_mwi; ?>
+
+<div id="mwi_right">
+	<div class="mwi_meta">
+
+		<h3><?php _e('Documentation', $jck_mwi->slug); ?></h3>
+		<p>Visit <a href="http://www.mwi-plugin.com" target="_blank">www.mwi-plugin.com</a> for all the latest documentation; including usage instructions, functions, examples and more.</p>
+
+		<h3><?php _e('Support', $jck_mwi->slug); ?></h3>
+		<p>If you require some support, please check out the <a href="http://mwi-plugin.com/documentation/faq/" target="_blank">FAQ section</a> on the MWI website. If all else fails, please <a href="mailto:support@mwi-plugin.com" target="_blank">send an email</a>.</p>
+
+		<h3><?php _e('Twitter', $jck_mwi->slug); ?></h3>
+		<a href="https://twitter.com/Gigasavvy" class="twitter-follow-button" data-show-count="false">Follow @Gigasavvy</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+	</div>
+</div><!-- /#mwi_right -->
